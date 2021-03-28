af4d8f3dd7defbc05945914ed3636ebff4cbc24a
RezervLZT <rostofficial@mail.ru>
2021-03-28 21:05:25 +0500

    lab 32

---
+++ lab32.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float f, h;
+  double i;
+  printf ("Vvedute shag -> h:");
+  scanf ("%f", &h);
+  printf ("\tx\t f(x)\n");
+  printf ("_____________________________\n");
+  for (i=0; i<2.1;i+=h)
+  {
+    if (i>=0 && i<=1)
+       f=sqrt(i+1)+sqrt(i)-1/2;
+    if (i>1 && i<=2)
+       f=pow(2.7182,-i-1/i);
+    printf("\tx%6.1f  f(x)%6.3f\n", i, f);
+  }
+}
