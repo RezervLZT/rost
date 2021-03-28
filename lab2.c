979047a4d4d425752783c6f296463e60e31dca51
RezervLZT <rostofficial@mail.ru>
2021-03-28 12:57:40 +0500

    NEW

--- lab2.c
+++ lab2.c
@@ -5,14 +5,14 @@ int main()
   double x,z;
   printf("Vvedute x -> [-3;0]");
   scanf ("%lf", &x);
-  if (x>=-10.0 && x<=0.0)
-{
+  if (x>=-3.0 && x<=0.0)
+  {
     double  y= (1.0/tan(x))+acos(pow(2,x));
     if (y<0) z=-1;
     if (y==0) z=0;
     if (y>0) z=1;
     printf ("y(x)= %lf\n z(y)= %lf\n", y,z);
-}
+  }
   else
     printf ("Zalupka\n");
   return 0;
